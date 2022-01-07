269007e939545bc7ce25718bfa0da820fb4bc39b
Alvin Aheebwa <alvinahe@programmer.net>
2022-01-07 21:16:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
+require("dotenv").config();
 
+var options = {
+  host: "ult10.falix.gg",
+  port: 38822, // default minecraft server port
+  username: "Alv-BOT",
+  version: "1.17.1"
+};
+
+module.exports = { options };
